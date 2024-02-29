6a6c923d6e0781605996b4b6ad269daf6846be98
fynma <rifkydian13@gmail.com>
2024-03-01 02:57:06 +0700

    selesai cuyy searchnyaaa

--- app/Http/Controllers/transactionController.php
+++ app/Http/Controllers/transactionController.php
@@ -10,23 +10,6 @@ class transactionController extends Controller
 {
     public function transaction_midtrans(Request $request)
     {
-        $data = $request->all();
-
-        $transaction = transaction::create([
-            'username' => $data['username'],
-            'denda_id' => $data['denda_id'],
-            'user_id' => $data['user_id'],
-            'total_denda' => $data['total_denda'],
-        ]);
-
-        // config('midtrans.serverKey')
-        \Midtrans\Config::$serverKey = 'SB-Mid-server-RriNo9Xmt9LiSgIMj7tdvhLt';
-        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
-        \Midtrans\Config::$isProduction = false;
-        // Set sanitization on (default)
-        \Midtrans\Config::$isSanitized = true;
-        // Set 3DS transaction for credit card to true
-        \Midtrans\Config::$is3ds = true;
 
         $params = array(
             'transaction_details' => array(
